a6ac52e23b15c0a7111157e9499885d5c81fdfb3
Brijesh Shah <brijeshjain13@gmail.com>
2021-10-27 21:03:15 +0530

    chore: add gin server with /health-check

--- main.go
+++ main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"github.com/brijeshshah13/url-shortener-service/models/dbs"
+	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	if err := initDBConn(dbs.DBNames["main"]); err != nil {
 		log.Fatal(err)
 	}
+
+	r := gin.Default()
+
+	r.GET("/health-check", func (ctx *gin.Context) {
+		ctx.String(http.StatusOK, "OK")
+	})
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	r.Run(":" + httpPort)
 }
 
 func initDBConn(dbName string) error {
